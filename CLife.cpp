60ce299cbea6426bdee9fe30ee0ab352aa75f6e2
GamePad <hejdjdjuduf@gmail.com>
2025-02-15 19:04:41 +0300

    Add files via upload
    Массив координат теперь динамический, что позволяет обрабатывать более большие данные, а так же переделана система буфферизации, что значительно ускорило просчет.

--- CLife.cpp
+++ CLife.cpp
@@ -3,8 +3,9 @@
 #include <time.h>
 #include <stdlib.h>
 
-const int sizeX = 650;
-const int sizeY = 250;
+const int sizeX = 960;
+const int sizeY = 450;
+const int randomChance = 5; //bigger value means a lower chance to spawn
 
 void gotoxy(short x,short y) { 
 	HANDLE StdOut = GetStdHandle(STD_OUTPUT_HANDLE); 
@@ -12,10 +13,22 @@ void gotoxy(short x,short y) {
 	SetConsoleCursorPosition(StdOut, coord); 
 }
 
+struct ConsoleBuffer {
+  HANDLE Console;
+  DWORD BytesWritten;
+
+  void Handle() {
+    Console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(Console);
+    BytesWritten = 0;
+  }
+};
+
 class Life {
 	private:
-		int area[sizeX][sizeY];
-		int updArea[sizeX][sizeY];
+		int** area;
+		int** updArea;
+		
 		int x;
 		int y;
 		int nearSum;
@@ -26,13 +39,35 @@ class Life {
 		
 		int bufferCounter;
 		char bufferArea[sizeX*sizeX];
+		
+		ConsoleBuffer cBuff;
 	public:
-		Life(int A1, int A2,int B) : x(0), y(0), nearSum(0), bufferCounter(0), A1(A1), A2(A2), B(B) {}
+		Life(int A1, int A2,int B) : x(0), y(0), nearSum(0), bufferCounter(0), A1(A1), A2(A2), B(B) {
+			area = new int*[sizeY];
+			updArea = new int*[sizeY];
+			
+			for (int i = 0; i < sizeY; i++) {
+			    area[i] = new int[sizeX];
+			    updArea[i] = new int[sizeX];
+			}
+			
+			cBuff.Handle();
+		}
+		
+		~Life() {
+			for (int i = 0; i < sizeY; i++) {
+			    delete[] area[i];
+			    delete[] updArea[i];
+			}
+			
+			delete[] area;
+			delete[] updArea;
+		}
 		void resetArea() {
 			for (y = 0; y < sizeY; y++) {
 				for (x = 0; x < sizeX; x++) {
-					area[x][y] = 0;
-					updArea[x][y] = 0;
+					area[y][x] = 0;
+					updArea[y][x] = 0;
 				}
 			}
 			
@@ -45,54 +80,37 @@ class Life {
 			srand(time(NULL));
 			for (y = 1; y < sizeY-1; y++) {
 				for (x = 1; x < sizeX-1; x++) {
-					if (rand() % 5 == 3) {
-						area[x][y] = rand() % 2;
+					if (rand() % randomChance == 1) {
+						area[y][x] = rand() % 2;
 					}
 				}
 			}
 		}
 		
-		/*
 		void draw() {
-			for (y = 1; y < sizeY-1; y++) {
-				for (x = 1; x < sizeX-1; x++) {
-					gotoxy(x,y);
-					if (area[x][y] == 1) {
-						printf("%c", 219);
-					}
-					else {
-						printf(" ");
-					}
-				}
-			}
-		}
-		*/
-		
-		void draw() {
-			gotoxy(0,0);
-			printf("%s", bufferArea);
+			WriteConsoleOutputCharacterA(cBuff.Console, bufferArea, sizeX * sizeY, { 0, 0 }, &cBuff.BytesWritten);
 		}
 
 		void updateArea() {
 			for (y = 1; y < sizeY-1; y++) {
 				for (x = 1; x < sizeX-1; x++) {
-					nearSum = area[x+1][y] + area[x-1][y] + area[x][y+1] + area[x][y-1] + area[x+1][y+1] + area[x-1][y-1] + area[x+1][y-1] + area[x-1][y+1];
+					nearSum = area[y+1][x] + area[y-1][x] + area[y][x+1] + area[y][x-1] + area[y+1][x+1] + area[y-1][x-1] + area[y+1][x-1] + area[y-1][x+1];
 					
-					if (area[x][y] == 1 && nearSum == A1 || area[x][y] == 1 && nearSum == A2) {
-						updArea[x][y] = 1;
+					if (area[y][x] == 1 && nearSum == A1 || area[y][x] == 1 && nearSum == A2) {
+						updArea[y][x] = 1;
 					}
 					else {
-						if (area[x][y] != 0) {
-							updArea[x][y] = 0;
+						if (area[y][x] != 0) {
+							updArea[y][x] = 0;
 						}
 					}
 					
-					if (area[x][y] == 0 && nearSum == B) {
-						updArea[x][y] = 1;
+					if (area[y][x] == 0 && nearSum == B) {
+						updArea[y][x] = 1;
 					}
 					else {
-						if (area[x][y] != 1) {
-							updArea[x][y] = 0;
+						if (area[y][x] != 1) {
+							updArea[y][x] = 0;
 						}
 					}
 				}
@@ -101,8 +119,8 @@ class Life {
 			bufferCounter = 0;
 			for (y = 0; y < sizeY; y++) {
 				for (x = 0; x < sizeX; x++) {
-					area[x][y] = updArea[x][y];
-					if (updArea[x][y] == 1){
+					area[y][x] = updArea[y][x];
+					if (updArea[y][x] == 1){
 						bufferArea[bufferCounter] = 219;
 					}
 					else {
@@ -115,6 +133,8 @@ class Life {
 };
 
 int main() {
+	//Do not change VVVV
+	
 	void* handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO structCursorInfo;
 	GetConsoleCursorInfo(handle,&structCursorInfo);
@@ -130,18 +150,20 @@ int main() {
     cfi.FontWeight = FW_NORMAL;
     wcscpy(cfi.FaceName, L"Consolas");
     SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+    
+    system("mode con cols=960 lines=450");
+    system("title Life");
+    
+	//Do not change /\/\/\/\
 	
 	Life game(2,3,3); //s s b - rules. Default "Life" params is 2 3 3
 	game.resetArea();
 	game.randomizeArea();
-
-	system("mode con cols=650 lines=250");
-	system("title Life");
 	
 	while (true) {
 		game.draw();
 		game.updateArea();
-		Sleep(25);
+		Sleep(10);
 	}
 	
 	return 0;
